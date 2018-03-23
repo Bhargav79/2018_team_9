ff92c9da3b78b795ebed29f513e89166fe5d61b4
Gavril Nanaziashvili <gavril.nanaziashvili@gmail.com>
2018-03-23 16:54:27 -0500

    fixed CORS

--- src/app.js
+++ src/app.js
@@ -3,9 +3,16 @@ import nutritionalScoreHandler from './nutritionalScoreHandler';
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.get('/', nutritionalScoreHandler);
 
 const port = process.env.PORT || 3000;
+
+
 app.listen(port);
 console.log('Node + Express REST API skeleton server started on port: ' + port);
 
