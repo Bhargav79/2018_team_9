0a337cea12520b9e1b99cc8a4734d54d79622fe3
Andreas <achristianson@sourceallies.com>
2018-03-23 10:49:19 -0500

    added output of all other nut facts to rules, in addition to contract interface

--- src/nutritionix/convertNutritionixToNutritionInfo.js
+++ src/nutritionix/convertNutritionixToNutritionInfo.js
@@ -2,4 +2,8 @@ export default async (raw) => ({
   sugar: raw.foods[0].nf_sugars,
   sodium: raw.foods[0].nf_sodium,
   fat: raw.foods[0].nf_total_fat,
+  protein: raw.foods[0].nf_protein,
+  fiber: raw.foods[0].nf_dietary_fiber,
+  ...raw.foods[0],
+  full_nutrients: null
 });
