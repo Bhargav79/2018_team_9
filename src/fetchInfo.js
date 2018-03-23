8511305610d07fb46ee14d883c84903afba6a4db
kickercost <joshua@kickercost.com>
2018-03-23 00:56:06 -0500

    working basline api integration for nutriotion fetching

--- src/fetchInfo.js
+++ src/fetchInfo.js
@@ -1,4 +1,4 @@
-import fetch from "fetch"
+import fetch from "node-fetch"
 import querystring from "querystring"
 //sample call
 // https://api.nal.usda.gov/ndb/search/?format=xml&q=**UPC**&max=25&offset=0&api_key=vOXgi2JypetmVvVEILas1K7rVieqQgr01nUptK19
@@ -7,28 +7,25 @@ let baseUrl = "https://api.nal.usda.gov/ndb/search/";
 let format = "json";
 let api_key = "vOXgi2JypetmVvVEILas1K7rVieqQgr01nUptK19";
 
-let fetcher = function (){
+let fetcher = async function (upc){
 
-
-  return querystring.stringify({
-      "category": "Starch",
-      "subCategory": "Rice",
-      "sodium": "10",
-      "sugar": "20",
-      "fat": "30"
-  });
-
-    // fetch.fetchUrl(baseUrl,)
 };
 
-export default (upc) => {
-
-     console.log(fetcher());
-      return {
-      "category": "Starch",
-      "subCategory": "Rice",
-      "sodium": "10",
-      "sugar": "20",
-      "fat": "30"
+export default async (upc) => {
+    let params = {
+        "api_key" : api_key,
+        "format":format,
+        "q":upc
     };
+    let call = baseUrl.concat("?").concat(querystring.stringify(params));
+    console.log(call);
+    return await fetch(call).then(res => res.json());
+
+    //   return {
+    //   "category": "Starch",
+    //   "subCategory": "Rice",
+    //   "sodium": "10",
+    //   "sugar": "20",
+    //   "fat": "30"
+    // };
 }
