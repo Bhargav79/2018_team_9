77ad9f2585a9ee6f40e0e33ce500d17b2ea69e39
Andreas <achristianson@sourceallies.com>
2018-03-23 01:27:04 -0500

    added check for not found

--- src/nutritionix/fetchNutritionInfo.js
+++ src/nutritionix/fetchNutritionInfo.js
@@ -7,5 +7,9 @@ import parseRawResult from './convertNutritionixToNutritionInfo'
 export default async (upc) => {
   const url = `https://api.nutritionix.com/v1_1/item?upc=${upc}&appId=${config.nutritionix.appId}&appKey=${config.nutritionix.appKey}`;
   const rawResult = await fetch(url).then(res => res.json());
-  return parseRawResult(rawResult);
-}
+  if (rawResult.status_code === 404) {
+    return undefined;
+  } else {
+    return parseRawResult(rawResult);
+  }
+};
