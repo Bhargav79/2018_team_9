f0a4cf353edc08fa0a9e632506c4d50bad42877b
Andreas <achristianson@sourceallies.com>
2018-03-25 14:42:31 -0500

    set up priority list of apis to be called. added a timeout for each call.

--- src/usda/fetchNutritionInfo.js
+++ src/usda/fetchNutritionInfo.js
@@ -3,12 +3,14 @@ import config from '../config/config';
 import convert from './convertUsdaToNutritionInfo';
 
 export default async (upc) => {
-  const firstPass = await fetch(`${config.usda.apiBaseUrl}?format=${config.usda.format}&max=${config.usda.max}&offset=${config.usda.offset}&q=${upc}&api_key=${config.usda.apiKey}`).then(res => res.json());
+  const searchUrl = `${config.usda.apiBaseUrl}?format=${config.usda.format}&max=${config.usda.max}&offset=${config.usda.offset}&q=${upc}&api_key=${config.usda.apiKey}`;
+  const firstPass = await fetch(searchUrl, config.usda.fetchOptions)
+    .then(res => res.json());
   if (firstPass.list && firstPass.list.item) {
     const ndbno = firstPass.list.item[0].ndbno;
-    const rawResult = await fetch(`${config.usda.apiNutritionUrl}?format=${config.usda.format}&ndbno=${ndbno}&api_key=${config.usda.apiKey}`).then(res => res.json());
+    const detailsUrl = `${config.usda.apiNutritionUrl}?format=${config.usda.format}&ndbno=${ndbno}&api_key=${config.usda.apiKey}`;
+    const rawResult = await fetch(detailsUrl, config.usda.fetchOptions)
+      .then(res => res.json());
     return convert(rawResult);
-  }else{
-    return undefined;
   }
 }
